97820c9b4c728ee9407dd549c262be048d949cf9
unknown <fadlullahyazid@gmail.com>
2019-09-24 23:23:30 +0700

    First commit

---
+++ src/pbo11k/pkg10118907/latihan9/teks/PBO11K10118907Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan9.teks;
+
+/**
+ *
+ * @author
+ * NAMA     : Yazid Fadlullah At-Taukily
+ * KELAS    : PBO11K
+ * NIM      : 10118907
+ * Deskripsi Program : Latihan teks
+ * 
+ */
+public class PBO11K10118907Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil"+newline+"char: "+tab+"berisi "+c);
+    }
+    
+}
